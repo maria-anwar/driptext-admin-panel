08caf9b4050ca6c5ffef814d7d1353cf809ceede
abubakarnangrii <abubakarnangri@gmail.com>
2024-08-21 04:42:39 +0500

    task details show on model

--- src/components/tables/ProjectTaskTable.tsx
+++ src/components/tables/ProjectTaskTable.tsx
@@ -3,13 +3,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFolder, faEye, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "antd/dist/reset.css";
 
-interface Worker {
-  texter: string;
-  lector: string;
-  SEO: string;
-  metalector: string;
-}
-
 interface Task {
   status: string;
   deadline: string;
@@ -62,6 +55,7 @@ interface ProjectProps {
 
 const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
   const [showDetailsDialog, setShowDetailsDialog] = useState(false);
+  const [task, setTask] = useState("");
 
   const getInitials = (name: string): string => {
     return name
@@ -85,12 +79,30 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
     );
   };
 
-  const handleTasks = () => {
+  const handleTasks = (task: any) => {
+    setTask(task);
     setShowDetailsDialog(true);
   };
 
   const hanldeCloseAllInfo = () => {
     setShowDetailsDialog(false);
+  };
+
+  const TaskMember: React.FC<{ label: string; name: string }> = ({
+    label,
+    name,
+  }) => {
+    return (
+      <div className="flex justify-between items-center py-2">
+        <div className="flex justify-center items-center">
+          <p className="text-black w-6 h-6 dark:text-white bg-slate-200 dark:bg-slate-600 rounded-full text-xs px-1 py-1 flex justify-center items-center">
+            {getInitials(name)}
+          </p>
+          <p className="pl-4">{name}</p>
+        </div>
+        <div>{label}</div>
+      </div>
+    );
   };
 
   return (
@@ -184,7 +196,7 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
                   <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                     <div
                       className={`bg-blue-500 w-24 h-9 flex justify-center items-center rounded cursor-pointer`}
-                      onClick={handleTasks}
+                      onClick={() => handleTasks(task)}
                     >
                       <FontAwesomeIcon className="text-white" icon={faEye} />
                       <p className="text-white text-base font-medium text-center py-1 px-2">
@@ -200,7 +212,7 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
       </div>
       {showDetailsDialog && (
         <div className="w-auto fixed inset-0 flex items-center justify-center z-[9999] bg-neutral-200 dark:bg-slate dark:bg-opacity-15 bg-opacity-60 px-4">
-          <div className="bg-white dark:bg-black p-6 rounded shadow-lg lg:w-5/12 xl:w-5/12 2xl:w-5/12 3xl:w-5/12 max-h-[90vh] overflow-y-auto scrollbar-hide">
+          <div className="bg-white dark:bg-black p-6 rounded shadow-lg lg:w-6/12 xl:w-6/12 2xl:w-6/12 3xl:w-6/12 max-h-[90vh] overflow-y-auto scrollbar-hide">
             <div className="flex justify-between items-center mb-2">
               <h2 className="text-xl font-bold dark:text-white">
                 Task Details
@@ -213,7 +225,153 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
             </div>
 
             <div className="space-y-4 mt-4">
-              <div>Feedback</div>
+              <div className="flex justify-between items-center w-full ">
+                <div className="w-1/2">
+                  <p className="mb-3 block text-sm font-medium text-black dark:text-white">
+                    Status
+                  </p>
+                  <p className="w-ful py-0  text-black dark:text-white">
+                    {task.status.toUpperCase()}
+                  </p>
+                </div>
+                <div className="w-1/2 flex justify-between items-center ">
+                  <div className="w-1/2 mr-1">
+                    <label
+                      className="mb-3 block text-sm font-medium text-black dark:text-white"
+                      htmlFor="wordReal"
+                    >
+                      Word Real
+                    </label>
+                    <p className="w-ful py-2  text-black dark:text-white">
+                      {task.wordCount}
+                    </p>
+                  </div>
+                  <div className="w-1/2 ml-1">
+                    <label
+                      className="mb-3 block text-sm font-medium text-black dark:text-white"
+                      htmlFor="wordExpected"
+                    >
+                      Word Expected
+                    </label>
+                    <input
+                      className="w-full rounded border border-transparent bg-gray py-2 px-4 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                      type="number"
+                      name="wordExpected"
+                      id="wordExpected"
+                      placeholder="1500"
+                      min={0}
+                      value={1500}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between items-center w-full ">
+                <div className="w-1/2 mr-1">
+                  <label
+                    className="mb-3 block text-sm font-medium text-black dark:text-white"
+                    htmlFor="dueUntil"
+                  >
+                    Due until
+                  </label>
+                  <p className="w-ful py-0  text-black dark:text-white">
+                    {task.deadline}
+                  </p>
+                </div>
+                <div className="w-1/2 ml-1">
+                  <label
+                    className="mb-3 block text-sm font-medium text-black dark:text-white"
+                    htmlFor="fullName"
+                  >
+                    Document
+                  </label>
+                  <p className="w-ful py-0  text-black dark:text-white">
+                    <a
+                      href={task.googleLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 underline-none"
+                    >
+                      <FontAwesomeIcon
+                        icon={faFolder}
+                        className="text-blue-500"
+                      />
+                      {""} {"MP-g649"}
+                    </a>
+                  </p>
+                </div>
+              </div>
+              <div className="w-full flex justify-between items-center">
+                <div className="w-1/2 mr-1">
+                  <label
+                    className="mb-3 block text-sm font-medium text-black dark:text-white"
+                    htmlFor="Topic"
+                  >
+                    Topic
+                  </label>
+                  <input
+                    className="w-full rounded border border-transparent bg-gray py-2 px-4 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                    type="text"
+                    name="Topic"
+                    id="Topic"
+                    placeholder="Topic"
+                    value={task.textType}
+                  />
+                </div>
+                <div className="w-1/2 ml-1">
+                  <label
+                    className="mb-3 block text-sm font-medium text-black dark:text-white"
+                    htmlFor="dropdown"
+                  >
+                    Text type
+                  </label>
+                  <select
+                    id="dropdown"
+                    className="w-full rounded border border-transparent bg-gray py-2.5 px-4 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                  >
+                    <option>Guide</option>
+                    <option>Shop (Category)</option>
+                    <option>Shop (Product)</option>
+                    <option>Definition/Wiki</option>
+                    <option>Shop (Home page)</option>
+                    <option>CMS page</option>
+                  </select>
+                </div>
+              </div>
+              <div className="w-full">
+                <label
+                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  htmlFor="Keywords"
+                >
+                  Keywords
+                </label>
+                <input
+                  className="w-full rounded border border-transparent bg-gray py-2 px-4 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                  type="text"
+                  name="Keywords"
+                  id="Keywords"
+                  placeholder="Keywords"
+                  defaultValue={""}
+                />
+              </div>
+              <div className="w-full">
+                <label
+                  className="mb-3 block text-sm font-medium text-black dark:text-white"
+                  htmlFor="comment"
+                >
+                  Comment
+                </label>
+                <textarea
+                  id="comment"
+                  className="w-full rounded border border-transparent bg-gray py-2 px-4 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                ></textarea>
+              </div>
+              <div className="w-full pt-6">
+                <h2>Members</h2>
+                <TaskMember name={task.worker.texter} label="Texter" />
+                <TaskMember name={task.worker.lector} label="Lector" />
+                <TaskMember name={task.worker.SEO} label="Seo" />
+                <TaskMember name={task.worker.metalector} label="Meta-Lector" />
+              </div>
             </div>
           </div>
         </div>
