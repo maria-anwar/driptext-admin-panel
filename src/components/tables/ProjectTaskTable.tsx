6c1d7dda5b5a035d0d5c0d573649ba5ec73ed114
abubakarnangrii <abubakarnangri@gmail.com>
2024-08-24 03:38:22 +0500

    icons added

--- src/components/tables/ProjectTaskTable.tsx
+++ src/components/tables/ProjectTaskTable.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFolder, faEye, faTimes,faCheck } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFolder,
+  faEye,
+  faTimes,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
 import "antd/dist/reset.css";
 
 interface Task {
@@ -102,8 +107,8 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
           <p className="pl-4">{name}</p>
         </div>
         <div className="flex justify-center items-center bg-green-600 rounded-full px-2 text-white">
-           <p className=" px-1">{label}</p>
-           <FontAwesomeIcon className=" px-1" icon={faCheck}/>
+          <p className=" px-1">{label}</p>
+          <FontAwesomeIcon className=" px-1" icon={faCheck} />
         </div>
       </div>
     );
@@ -157,18 +162,73 @@ const ProjectTaskTable: React.FC<ProjectProps> = ({ tasks, project }) => {
                       href={task.googleLink}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-blue-500 underline-none"
+                      className="text-blue-500 underline-none flex items-center justify-start"
                     >
-                      <FontAwesomeIcon
-                        icon={faFolder}
-                        className="text-blue-500"
-                      />
-                      {""} {"MP-g649"}
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-5.5 h-5.5"
+                      >
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <path
+                            d="M20 4L12 12M20 4V8.5M20 4H15.5M19 12.5V16.8C19 17.9201 19 18.4802 18.782 18.908C18.5903 19.2843 18.2843 19.5903 17.908 19.782C17.4802 20 16.9201 20 15.8 20H7.2C6.0799 20 5.51984 20 5.09202 19.782C4.71569 19.5903 4.40973 19.2843 4.21799 18.908C4 18.4802 4 17.9201 4 16.8V8.2C4 7.0799 4 6.51984 4.21799 6.09202C4.40973 5.71569 4.71569 5.40973 5.09202 5.21799C5.51984 5 6.07989 5 7.2 5H11.5"
+                            stroke="#3b82f6"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </g>
+                      </svg>
+                      <span className="px-1">{"MP-g649"}</span>
                     </a>
                   </td>
                   <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                    <p className="text-white bg-red-500 px-2 rounded-full text-center">
-                      {task.deadline}
+                    <p className="text-white bg-red-500 px-1 rounded-full text-center flex justify-center items-center">
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="w-4 h-4"
+                      >
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        ></g>
+                        <g id="SVGRepo_iconCarrier">
+                          {" "}
+                          <path
+                            d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
+                            stroke="#FFFFFF"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>{" "}
+                          <path
+                            d="M12 6V12"
+                            stroke="#FFFFFF"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>{" "}
+                          <path
+                            d="M16.24 16.24L12 12"
+                            stroke="#FFFFFF"
+                            stroke-width="1.5"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>{" "}
+                        </g>
+                      </svg>
+                      <span className="px-1">{task.deadline}</span>
                     </p>
                   </td>
                   <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
