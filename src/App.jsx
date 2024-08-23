7bb11e59a5d4488385c3eb6cc10fe67a876167cf
abubakarnangrii <abubakarnangri@gmail.com>
2024-08-23 05:46:15 +0500

    accordion added on the project deatils using chakar library

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import WebRoutes from './routes'
 import { ThemeProvider } from '@material-tailwind/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
 
   return (
     <ThemeProvider>
+    <ChakraProvider>
         <WebRoutes/>
+        </ChakraProvider>
     </ThemeProvider>
 
   )
