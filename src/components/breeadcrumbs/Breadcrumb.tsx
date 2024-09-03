27cb1aacd3b31676d05a17bb351375dfff51c436
abubakarnangrii <abubakarnangri@gmail.com>
2024-09-04 02:45:59 +0500

    Breadcrumb add

--- src/components/breeadcrumbs/Breadcrumb.tsx
+++ src/components/breeadcrumbs/Breadcrumb.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 interface BreadcrumbProps {
   pageName: string;
 }
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
-        {pageName}
-      </h2>
-
+    <div className="mb-6 flex flex-col gap-3  items-start justify-start">
       <nav>
         <ol className="flex items-center gap-2">
           <li>
-            <Link className="font-medium text-black hover:text-black dark:text-bodydark dark:hover:text-bodydark" to="/dashboard">
+            <Link
+              className="font-medium text-black hover:text-black dark:text-bodydark dark:hover:text-bodydark"
+              to="/dashboard"
+            >
               Dashboard /
             </Link>
           </li>
           <li className="font-medium text-primary">{pageName}</li>
         </ol>
       </nav>
+      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+        {pageName}
+      </h2>
     </div>
   );
 };
