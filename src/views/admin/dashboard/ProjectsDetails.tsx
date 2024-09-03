46572e82ea061d940792f28bb8262b25553f63d6
abubakarnangrii <abubakarnangri@gmail.com>
2024-09-04 03:54:59 +0500

    delte icon change and also project detauils buttons color change

--- src/views/admin/dashboard/ProjectsDetails.tsx
+++ src/views/admin/dashboard/ProjectsDetails.tsx
@@ -8,6 +8,7 @@ import {
   faTimes,
   faCloudUploadAlt,
   faGripLines,
+  faTrashAlt
 } from "@fortawesome/free-solid-svg-icons";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -563,7 +564,7 @@ const ProjectsDetails: React.FC = () => {
                   )}
                   <button
                     onClick={handleEdit}
-                    className="w-full h-10 text-center bg-slate-300 text-blue-500 rounded-none my-2 flex justify-center items-center border-none"
+                    className="w-full h-10 text-center bg-slate-100 text-blue-500 hover:bg-blue-500 hover:text-white rounded-none my-2 flex justify-center items-center border-none"
                   >
                     Edit
                     <FontAwesomeIcon icon={faEdit} className="text-sm px-2" />
@@ -714,7 +715,7 @@ const ProjectsDetails: React.FC = () => {
                   <div className="w-full flex justify-between items-center my-1">
                     <button
                       onClick={handleImport}
-                      className="w-1/2 h-10 text-center bg-slate-300 text-blue-500 rounded-none mr-1.5 flex justify-center items-center border-none"
+                      className="w-1/2 h-10 text-center bg-slate-100 text-blue-500 hover:bg-blue-500 hover:text-white rounded-none mr-1.5 flex justify-center items-center border-none"
                     >
                       Import
                       <FontAwesomeIcon
@@ -767,7 +768,7 @@ const ProjectsDetails: React.FC = () => {
                       onClick={() => {
                         alert("download");
                       }}
-                      className="w-1/2 h-10 text-center bg-slate-300 text-blue-500 rounded-none ml-1.5 flex justify-center items-center border-none"
+                      className="w-1/2 h-10 text-center bg-slate-100 text-blue-500 hover:bg-blue-500 hover:text-white rounded-none ml-1.5 flex justify-center items-center border-none"
                     >
                       Export
                       <FontAwesomeIcon
@@ -778,41 +779,10 @@ const ProjectsDetails: React.FC = () => {
                   </div>
                   <button
                     onClick={handleDelete}
-                    className="w-full h-10 text-center bg-slate-300 text-slate-600 rounded-none my-2 flex justify-center items-center border-none"
+                    className="w-full h-10 text-center bg-slate-100 text-slate-600 hover:bg-blue-500 hover:text-white rounded-none my-2 flex justify-center items-center border-none"
                   >
                     <p className="px-2">Archive Project</p>
-                    <svg
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-4 h-5"
-                    >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                      <g
-                        id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      ></g>
-                      <g id="SVGRepo_iconCarrier">
-                        <path
-                          d="M9 12C9 11.5341 9 11.3011 9.07612 11.1173C9.17761 10.8723 9.37229 10.6776 9.61732 10.5761C9.80109 10.5 10.0341 10.5 10.5 10.5H13.5C13.9659 10.5 14.1989 10.5 14.3827 10.5761C14.6277 10.6776 14.8224 10.8723 14.9239 11.1173C15 11.3011 15 11.5341 15 12C15 12.4659 15 12.6989 14.9239 12.8827C14.8224 13.1277 14.6277 13.3224 14.3827 13.4239C14.1989 13.5 13.9659 13.5 13.5 13.5H10.5C10.0341 13.5 9.80109 13.5 9.61732 13.4239C9.37229 13.3224 9.17761 13.1277 9.07612 12.8827C9 12.6989 9 12.4659 9 12Z"
-                          stroke="#000000"
-                          stroke-width="1"
-                        ></path>
-                        <path
-                          opacity="0.5"
-                          d="M20.5 7V13C20.5 16.7712 20.5 18.6569 19.3284 19.8284C18.1569 21 16.2712 21 12.5 21H11.5C7.72876 21 5.84315 21 4.67157 19.8284C3.5 18.6569 3.5 16.7712 3.5 13V7"
-                          stroke="#000000"
-                          stroke-width="1"
-                          stroke-linecap="round"
-                        ></path>
-                        <path
-                          d="M2 5C2 4.05719 2 3.58579 2.29289 3.29289C2.58579 3 3.05719 3 4 3H20C20.9428 3 21.4142 3 21.7071 3.29289C22 3.58579 22 4.05719 22 5C22 5.94281 22 6.41421 21.7071 6.70711C21.4142 7 20.9428 7 20 7H4C3.05719 7 2.58579 7 2.29289 6.70711C2 6.41421 2 5.94281 2 5Z"
-                          stroke="#000000"
-                          stroke-width="1"
-                        ></path>
-                      </g>
-                    </svg>
+                    <FontAwesomeIcon icon={faTrashAlt} />
                   </button>
                   {deleteModel && (
                     <DeleteModel
