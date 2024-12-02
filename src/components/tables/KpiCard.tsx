68a60cccab72e3a31d1c14ea5b13b43965779638
abubakarnangrii <abubakarnangri@gmail.com>
2024-12-03 00:48:47 +0500

    gui changes inn kpi

--- src/components/tables/KpiCard.tsx
+++ src/components/tables/KpiCard.tsx
@@ -5,6 +5,11 @@ import {
   faChartBar,
   faUsers,
   faRocket,
+  faDollarSign,
+  faCreditCard,
+  faPiggyBank,
+  faChartLine,
+  faEuroSign
 } from "@fortawesome/free-solid-svg-icons";
 import "antd/dist/reset.css";
 import "./custompagination.css";
@@ -22,20 +27,27 @@ interface KpiCardProps {
 }
 
 // Reusable component for KPI card
-const KpiBlock: React.FC<{ icon: React.ComponentProps<typeof FontAwesomeIcon>['icon'], title: string, value: number }> = ({ icon, title, value }) => {
+const KpiBlock: React.FC<{
+  icon: React.ComponentProps<typeof FontAwesomeIcon>["icon"];
+  title: string;
+  value: number;
+}> = ({ icon, title, value }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white px-5 py-5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="flex items-center gap-8 px-2 py-2">
+    <div className="flex items-center gap-4 rounded-sm border border-stroke bg-white pl-6 pr-2 py-8 shadow-default dark:border-strokedark dark:bg-boxdark">
         <div>
           <p className="bg-slate-200 dark:bg-boxdark-2 w-12 h-12 rounded-full flex justify-center items-center">
-            <FontAwesomeIcon icon={icon} className="text-black dark:text-white w-6 h-6" />
+            <FontAwesomeIcon
+              icon={icon}
+              className="text-black dark:text-white w-6 h-6"
+            />
           </p>
         </div>
         <div>
           <p className="text-black dark:text-white">{title}</p>
-          <h1 className="text-black dark:text-white font-semibold text-2xl">€ {value}</h1>
+          <h1 className="text-black dark:text-white font-semibold text-2xl">
+            € {value}
+          </h1>
         </div>
-      </div>
     </div>
   );
 };
@@ -49,16 +61,32 @@ const KpiCard: React.FC<KpiCardProps> = ({
   revenue,
   margin,
 }) => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   return (
     <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 4xl:grid-cols-4 gap-x-5 gap-y-5">
-      <KpiBlock icon={faMoneyBillWave} title={t('kpi.kpiBlocks.0')} value={texter} />
-      <KpiBlock icon={faMoneyBillWave} title={t('kpi.kpiBlocks.1')} value={lector} />
-      <KpiBlock icon={faMoneyBillWave} title={t('kpi.kpiBlocks.2')} value={seo} />
-      <KpiBlock icon={faMoneyBillWave} title={t('kpi.kpiBlocks.3')} value={meta} />
-      <KpiBlock icon={faRocket} title={t('kpi.kpiBlocks.4')} value={revenue} />
-      <KpiBlock icon={faUsers} title={t('kpi.kpiBlocks.5')} value={totalCost} />
-      <KpiBlock icon={faChartBar} title={t('kpi.kpiBlocks.6')} value={margin} />
+      <KpiBlock
+        icon={faMoneyBillWave}
+        title={t("kpi.kpiBlocks.0")}
+        value={texter}
+      />
+      <KpiBlock
+        icon={faMoneyBillWave}
+        title={t("kpi.kpiBlocks.1")}
+        value={lector}
+      />
+      <KpiBlock
+        icon={faMoneyBillWave}
+        title={t("kpi.kpiBlocks.2")}
+        value={seo}
+      />
+      <KpiBlock
+        icon={faMoneyBillWave}
+        title={t("kpi.kpiBlocks.3")}
+        value={meta}
+      />
+      <KpiBlock icon={faRocket} title={t("kpi.kpiBlocks.4")} value={revenue} />
+      <KpiBlock icon={faEuroSign} title={t("kpi.kpiBlocks.5")} value={totalCost} />
+      <KpiBlock icon={faChartBar} title={t("kpi.kpiBlocks.6")} value={margin} />
     </div>
   );
 };
